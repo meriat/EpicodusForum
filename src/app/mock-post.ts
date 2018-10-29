269082dca407b50e49fee74ce4b65f3075dfa469
Meria Thomas and Skye Nguyen <skye@>
2018-10-29 15:15:06 -0700

    temporarily take out authentication buttons and add a route to the Epicodus Forum title

--- src/app/mock-post.ts
+++ src/app/mock-post.ts
@@ -2,7 +2,9 @@ import {Post} from './models/post-model';
 
 export const POSTS: Post[] = [
     new Post('New Student Introduction','Bla bla bla',0,1),
-    new Post('We are moving!','Bla bla bla',1,0),
+    new Post('We are moving!','Bla bla bla1',1,0),
+    new Post('We are moving!','Bla bla bla2',6,0),
+    new Post('We are moving!','Bla bla bla3',7,0),
     new Post('Looking for a job','Bla bla bla',2,4),
     new Post('Picnic','Bla bla bla',3,2),
     new Post('Referral','Bla bla bla',4,3),
